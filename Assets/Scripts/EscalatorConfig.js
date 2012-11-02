adec22d04d2ab36ab114f101476dcdf335cd560b
Keijiro Takahashi <keijiro@gmail.com>
2012-11-02 17:35:31 +0900

    Tweaked lightmapping config, and added manPerMinute.

--- Assets/Scripts/EscalatorConfig.js
+++ Assets/Scripts/EscalatorConfig.js
@@ -3,6 +3,7 @@
 var fastForwardRatio = 0.3;
 var escalatorSpeed = 0.8;
 var fastForwardSpeed = 1.5;
+var manPerMinute = 60.0;
 
 static var instance : EscalatorConfig;
 
--- Assets/Scripts/EscalatorGenerator.js
+++ Assets/Scripts/EscalatorGenerator.js
@@ -5,11 +5,12 @@ var radius = 5.0;
 var interval = 1.0;
 
 function Start() {
+	var config = EscalatorConfig.instance;
 	while (true) {
 		var phi = Random.value * Mathf.PI * 2;
 		var pos = Vector3(Mathf.Sin(phi), 0.0, Mathf.Cos(phi));
 		var instance = Instantiate(prefab, pos * radius, Quaternion.identity) as GameObject;
-		instance.GetComponent.<EscalatorAI>().fastForward = (Random.value < EscalatorConfig.instance.fastForwardRatio);
-		yield WaitForSeconds(interval);
+		instance.GetComponent.<EscalatorAI>().fastForward = (Random.value < config.fastForwardRatio);
+		yield WaitForSeconds(60.0 / config.manPerMinute);
 	}
 }
