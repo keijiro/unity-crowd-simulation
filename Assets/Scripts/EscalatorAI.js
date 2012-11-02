b94a37da39dee627a6dd27cbda7fed2a07b6fde0
Keijiro Takahashi <keijiro@gmail.com>
2012-11-02 16:07:22 +0900

    Refined movement on escalator.

--- Assets/Scripts/EscalatorAI.js
+++ Assets/Scripts/EscalatorAI.js
@@ -1,21 +1,36 @@
 #pragma strict
 
-private var agent : NavMeshAgent;
-
-function Awake() {
-	agent = GetComponent.<NavMeshAgent>();
-}
+var escalatorSpeed = 1.1;
 
 function Start() {
-	var goal = GameObject.Find("Goal " + Random.Range(1, 3));
-	agent.SetDestination(goal.transform.position);
-}
-
-function Update() {
+	var agent = GetComponent.<NavMeshAgent>();
 	var animator = GetComponent.<Animator>();
-	animator.SetFloat("speed", agent.velocity.magnitude);
-}
 
-function OnTriggerEnter(other : Collider) {
+	agent.updateRotation = false;
+
+	var entrance = GameObject.Find("Entrance " + Random.Range(1, 3)).transform;
+	agent.SetDestination(entrance.position);
+
+	yield;
+
+	while (agent.remainingDistance > 0.1) {
+		animator.SetFloat("speed", agent.velocity.magnitude);
+		if (agent.velocity.magnitude > 0.1) {
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(agent.velocity), 360.0 * Time.deltaTime);
+		}
+		yield;
+	}
+
+	agent.enabled = false;
+	animator.SetFloat("speed", 0);
+
+	var target = entrance.Find("Target");
+
+	while ((target.position - transform.position).magnitude > 0.1) {
+		transform.position = Vector3.MoveTowards(transform.position, target.position, escalatorSpeed * Time.deltaTime);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(180, Vector3.up), 30 * Time.deltaTime);
+		yield;
+	}
+
 	Destroy(gameObject);
 }
